78827490d092254b9afa52e4a62c0ee6f535f451
JP Sio <hello@jpsio.me>
2021-10-26 16:31:01 -0700

    all hail apple polishing cloth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          className="responsive"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MM6F3?wid=1144&hei=1144&fmt=jpeg&qlt=95&.v=1632956535000"
+          alt="logo"
+        />
       </header>
     </div>
   );
